a1e1cf977fc7208fcb2669450f54ffee1a22404b
Agrim-Saxena <agrim1312saxena@gmail.com>
2024-04-19 01:31:38 +0530

    first commit

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "ec7e2efdf3cbf56d1f58cdc878f33b05";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&languages=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&languages=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanticMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
